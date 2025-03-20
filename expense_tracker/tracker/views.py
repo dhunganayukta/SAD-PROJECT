a82bac97a8be178d71e6f9c7c03ef9562e8eee77
Yukta <yuktadhungana8@gmail.com>
2025-03-20 22:43:00 +0545

    updates

--- expense_tracker/tracker/views.py
+++ expense_tracker/tracker/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from .models import Transaction
 
 
 def register(request):
@@ -13,9 +16,7 @@ def register(request):
     return render(request, 'registration/register.html', {'form': form})
 
 
-from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
-from .models import Transaction
+
 
 @login_required
 def dashboard(request):
@@ -28,6 +29,25 @@ def dashboard(request):
 
 @login_required
 def add_transaction(request):
+    if request.method == 'POST':
+
+        amount = request.POST.get('amount')
+        category = request.POST.get('category')
+        date = request.POST.get('date')
+        from .models import Category
+        category_object , created = Category.objects.get_or_create(name=category)
+
+        transaction = Transaction(
+            user=request.user,
+            amount=amount,
+            category=category_object,
+            date=date,
+            notes = ''
+        )
+
+        transaction.save()
+        return redirect('dashboard')
+
     return render(request, 'tracker/addTransaction.html')
 
 def index(request):
